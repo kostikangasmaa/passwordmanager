77de49882857637b253511326e23919781648407
Kosti Kangasmaa <kangasmaa95@gmail.com>
2025-05-09 15:25:25 +0300

    converted app.json to app.config.js to work with dotenv for environment variables

---
+++ app.config.js
@@ -0,0 +1,35 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "passwordmanager",
+    slug: "passwordmanager",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY, 
+    },
+  },
+};
